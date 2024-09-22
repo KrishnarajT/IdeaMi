17eafd84d8559534784eef673b30a68e6a7b1d09
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2024-09-22 21:18:36 +0530

    added courses list to new user

--- src/app/models/UserModel.ts
+++ src/app/models/UserModel.ts
@@ -3,6 +3,7 @@ export class UserModel {
     public id: string = '',
     public name: string = '',
     public email: string = '',
-    public password: string = ''
+    public password: string = '',
+    public courses: number[] = []
   ) {}
 }
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -32,35 +32,35 @@ export class SignupComponent {
       const { name, email, password } = this.signupFormGroup.value;
       this.userServiceObj.fetchAllUsers().then((response) => {
         console.log(response);
-      });
 
-      // if the user exists, then redirect to login page
-      // else add the user and redirect to login page
-      console.log('User Name: ', this.user);
-      if (!name || !email || !password) {
-        alert('Please fill all the details');
-        return;
-      }
-
-      if (this.userServiceObj.allUsers.length > 0) {
-        const userExists = this.userServiceObj.allUsers.find(
-          (user) => user.email === email
-        );
-        if (userExists) {
-          alert('User already exists');
+        // if the user exists, then redirect to login page
+        // else add the user and redirect to login page
+        console.log('User Name: ', this.user);
+        if (!name || !email || !password) {
+          alert('Please fill all the details');
           return;
         }
-      }
 
-      this.userServiceObj
-        .addUser({ id: this.userId, name, email, password })
-        .then((response) => {
-          console.log('User added successfully');
-          this.router.navigate(['/login']);
-        })
-        .catch((error) => {
-          console.error('Error adding user:', error);
-        });
+        if (this.userServiceObj.allUsers.length > 0) {
+          const userExists = this.userServiceObj.allUsers.find(
+            (user) => user.email === email
+          );
+          if (userExists) {
+            alert('User already exists');
+            return;
+          }
+        }
+
+        this.userServiceObj
+          .addUser({ id: this.userId, name, email, password, courses: [] })
+          .then((response) => {
+            console.log('User added successfully');
+            this.router.navigate(['/login']);
+          })
+          .catch((error) => {
+            console.error('Error adding user:', error);
+          });
+      });
     } else {
       alert('Please fill all the details correctly');
     }
